69dd6b2fa13b13637eaabb2ac1ec08a933ffe1c7
kawasaki <kawasaki@kawasaki-no-MacBook-Pro.local>
2017-06-30 02:39:34 +0900

    add

--- create_mds.py
+++ create_mds.py
@@ -10,8 +10,9 @@ from time import time
 if __name__ == '__main__':
 	distance1 = np.load("distance1.npy")
 	distance2 = np.load("distance2.npy")
+	distance1 = np.load("point1.npy")
+	distance2 = np.load("point2.npy")
 
 	point1    = np.random.rand(distance1.shape[0])*distance1.max()
 	point2    = np.random.rand(distance2.shape[0])*distance2.max()
 
-	
