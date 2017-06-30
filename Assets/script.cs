19f8e38e0dab697d78f72d86f5b78de107a7eab7
kawasaki <t.mimura@em.ci.ritsumei.ac.jp>
2017-06-30 17:06:20 +0900

    add

--- Assets/script.cs
+++ Assets/script.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class script : MonoBehaviour {
     public GameObject name1;
     public GameObject name2;
+    public GameObject name3;
+    public GameObject name4;
 
     public Rigidbody name1_body;
     public Rigidbody name2_body;
+    public Rigidbody name3_body;
+    public Rigidbody name4_body;
 
     int t = 0;
     // Use this for initialization
     void Start () {
         name1_body = name1.GetComponent<Rigidbody>();
         name2_body = name2.GetComponent<Rigidbody>();
+        name3_body = name3.GetComponent<Rigidbody>();
+        name4_body = name4.GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
@@ -25,8 +31,10 @@ public class script : MonoBehaviour {
     {
         if (t % 10 == 0)
         {
-            name1_body.AddTorque(new Vector3(1,0,0) * Random.Range(-150.0f, 150.0f));
-            name2_body.AddTorque(new Vector3(1,0,0) * Random.Range(-150.0f, 150.0f));
+            name1_body.AddTorque(new Vector3(1, 0, 0) * Random.Range(-150.0f, 150.0f));
+            name2_body.AddTorque(new Vector3(1, 0, 0) * Random.Range(-150.0f, 150.0f));
+            name3_body.AddTorque(new Vector3(1, 0, 0) * Random.Range(-150.0f, 150.0f));
+            name4_body.AddTorque(new Vector3(1, 0, 0) * Random.Range(-150.0f, 150.0f));
         }
     }
 
